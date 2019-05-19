cc8b0318dd9b4b56e6af8f2a4319df2484a86de2
Joakim Sørensen <ludeeus@gmail.com>
2019-05-20 01:08:23 +0200

    Delete helpers.py

--- custom_components/hacs/helpers.py
+++
@@ -1,8 +0,0 @@
-"""Helper functions for HACS."""
-import asyncio
-
-
-async def run_tasks(tasks):
-    """Run tasks in background."""
-    for task in asyncio.as_completed(tasks):
-        await task
